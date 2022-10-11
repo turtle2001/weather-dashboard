dc1309075e5646c75d444f001ddd12d7f4e0fb42
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-10-11 07:57:37 -0700

    cleaned up html

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var search = $("#user-form")
 var city = $("#city-input")
-var bigBox = $("#main-container")
 
 function formSubmitHandler(event) {
     event.preventDefault();
@@ -40,15 +39,23 @@ function fillContainer(data, x) {
     else
         var cityEl = $("<h4></h4>").text(data.city.name + moment(data.list[x * 8].dt_txt).format(' (MM/D/YYYY)'));
 
-
     var tempEl = $("<p></p>").text("Temperature: " + data.list[x].main.temp + " degrees");
     var windEl = $("<p></p>").text("Wind: " + data.list[x].wind.speed + " MPH");
     var humidityEl = $("<p></p>").text("Humidity: " + data.list[x].main.humidity + " %");
 
     if (x == 0)
-        bigBox.append(cityEl, tempEl, windEl, humidityEl);
+        $("#main-container").append(cityEl, tempEl, windEl, humidityEl);
     else
         $("#mini-container" + x).append(cityEl, tempEl, windEl, humidityEl);
+
+    //addButton(cityEl);
 }
+
+// function addButton(name) {
+//     var buttonEl = $("<button>", { "class": "btn btn-secondary col-12 my-1" });
+//     buttonEl.text(name);
+//     buttonEl.attr("data-city-name", name);
+//     buttonListEl.prepend(buttonEl);
+// }
 
 search.click(formSubmitHandler);
